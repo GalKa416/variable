7b878635974520eaf00368babc440b87515f41ad
Галина <g416@mail.ru>
2023-03-21 23:39:14 +0500

    домашнее задание по переменным сделано

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.5;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+var friend = 19;
+System.out.println(friend);
+friend = friend + 2;
+System.out.println(friend);
+friend = friend / 7;
+System.out.println(friend);
+var frog = 3.5;
+System.out.println(frog);
+frog = frog * 10;
+System.out.println(frog);
+frog = frog / 3.5;
+System.out.println(frog);
+frog = frog + 4;
+System.out.println(frog);
+var MOneBoxer = 78.2;
+var MTwoBoxer = 82.7;
+var MBoxers = MOneBoxer + MTwoBoxer;
+System.out.println("Общий вес боксеров " + MBoxers + " кг");
+var difference = MTwoBoxer - MOneBoxer;
+System.out.println("Второй боксер весит на " + difference + " кг больше первого");
+var remainder = (MOneBoxer + MTwoBoxer) % MOneBoxer;
+System.out.println("разница в весе двух боксеров составляет " + remainder + " кг");
+var tWork = 640;
+var time = 8;
+var people = tWork / time;
+System.out.println("Всего работников в компании " + people+ " человек");
+people = people +94;
+tWork = time * people;
+System.out.println( "Если в компании работает " + people + " человек, то всего " + tWork + " часов работы поделено между сотрудниками");
+    }
+}
