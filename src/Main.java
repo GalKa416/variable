f2977ac5492e89888de3b883715aac572865c261
Галина <g416@mail.ru>
2023-04-01 17:10:34 +0500

    задача 4 готова

--- src/Main.java
+++ src/Main.java
@@ -18,16 +18,19 @@ public class Main {
             if (clientDeviceYear < 2015)
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 1) System.out.println("Установите версию приложения для Android по ссылке");
-
-
         // task 3;
         System.out.println("Задача 3");
         int year = 2000;
         if (year % 4 == 0 && year % 100!=0 || year % 400==0) {System.out.println("Год является високосным");}
         else System.out.println("Год не является високосным");
-
         //task 4;
         System.out.println("Задача 4");
+        int deliveryDistance = 95;
+        int deliveryTime = 1;
+        if (deliveryDistance<=20) {System.out.println("Доставка будет в течении "+deliveryTime+ " суток");}
+        else if (deliveryDistance>20 && deliveryDistance <= 60) {System.out.println("Доставка будет в течении "+deliveryTime*2+ " суток");}
+        else if (deliveryDistance > 60 && deliveryDistance <=100) {System.out.println("Доставка будет в течении " + deliveryTime*3 + " суток");}
+        else if (deliveryDistance>100) {System.out.println("Доставки не будет");}
      }
 }
 
