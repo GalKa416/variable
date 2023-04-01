edbf71d79e3f2017c87fc2b6765e1bd790ade5d9
Галина <g416@mail.ru>
2023-04-01 16:07:10 +0500

    задача 1 корректировка

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,38 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Условный оператор. Урок 1");
-        //task 1;
-
-//task 2;
-
-//task 3;
-
-//task 5.
-        //task 6;
-        System.out.println("Задача 6");
-        int NumberOfPassengers = 63;
-        if (NumberOfPassengers < 60) {
-            System.out.println("В вагоне есть сидячее место");
-        } else if (NumberOfPassengers >= 60 && NumberOfPassengers < 102) {
-            System.out.println("В вагоне сидячих мест нет, но есть стоячее");
-        } else if (NumberOfPassengers >= 102) {
-            System.out.println("В вагоне мест нет");
-        }
-
-        //task 7;
 
         System.out.println("Условный оператор. Урок 2");
         //task 1;
+        System.out.println("Задача 1");
+        int clientOS = 0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Некорректный ввод");
 
-        //task 2;
+            //task 2;
 
-        // task 3;
+            // task 3;
 
 
-        // tasr 4;
-        System.out.println("Задача 4");
-        int deliveryDistance = 95;
-        int deliveryTime = 1;
-        if (deliveryDistance <= 20) {
-            System.out.println("Доставка будет в течении " + deliveryTime + " суток");
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Доставка будет в течении " + deliveryTime + 1 + " суток");
-        }else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Доставка будет в течении " + deliveryTime + 2 + " суток");
-        }else if (deliveryDistance > 100 ) {
-            System.out.println("Доставки нет");
+            // task 4;
+            System.out.println("Задача 4");
+            int deliveryDistance = 95;
+            int deliveryTime = 1;
+            if (deliveryDistance <= 20) {
+                System.out.println("Доставка будет в течении " + deliveryTime + " суток");
+            } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+                System.out.println("Доставка будет в течении " + deliveryTime + 1 + " суток");
+            } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+                System.out.println("Доставка будет в течении " + deliveryTime + 2 + " суток");
+            } else if (deliveryDistance > 100) {
+                System.out.println("Доставки нет");
+            }
+            // task 5;
+            System.out.println("Задача 5");
         }
     }
 }
