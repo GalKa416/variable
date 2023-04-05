f9110ded937bbf9cfc5118dffe7d2d33c6db6996
Галина <g416@mail.ru>
2023-04-05 23:00:43 +0500

    задача 1 готова

--- src/Main.java
+++ src/Main.java
@@ -2,72 +2,17 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Циклы");
+
+
+        System.out.println("Циклы 2");
         //task 1;
         System.out.println("Задача 1");
-        for (int i = 0; i <= 10; i = i + 1) {
-            System.out.println(" Число " + i);
-        }
-//task 2;
-        System.out.println("Задача 2");
-        for (int i = 10; i <= 10 && i >= 1; i = i - 1) {
-            System.out.println(" Число " + i);
-        }
-        //task 3;
-        System.out.println("Задача 3");
-        for (int i = 0; i <= 17; i = i + 2) {
-            System.out.println(" Число " + i);
-        }
-        //task 4;
-        System.out.println("Задача 4");
-        for (int i = 10; i <= 10 && i >= -10; i = i - 1) {
-            System.out.println(" Число " + i);
-        }
-//task 5;
-        System.out.println("Задача 5");
-        for (int i = 1904; i <= 2096; i = i + 4) {
-            System.out.println(+i + " год является високосным. ");
-        }
-        //task 6;
-        System.out.println("Задача 6");
-        for (int i = 1; i <= 512; i = i * 2) {
-            System.out.println(" Число " + i);
-        }
-        //task 7;
-        System.out.println("Задача 7");
-        for (int i = 7; i <= 98; i = i + 7) {
-            System.out.println(" Число " + i);
-        }
-        //task 8;
-        System.out.println("Задача 8");
+        int salary = 15000;
         int total = 0;
-
-        int amountOfSavings = 29000;
-        for (int z  = 0; z < 12; z++){
-            total = total + amountOfSavings;}
-
-                        System.out.println("сумма накоплений равна " + total + " рублей.");
-
-        //task 9;
-        System.out.println("Задача 9");
-        for (int z  = 0; z <= 12; z++){
-            total = total + total/100;
-            total = total + amountOfSavings;
-            System.out.println("Месяц " + z + " Итого " + total);
-        }
-        System.out.println("сумма накоплений равна " + total + " рублей.");
-
-
-        //task 10;
-        System.out.println("Задача 10");
-        int a;
-        int b = 2;
-        for (int m  = 1; m <= 10;m++ ){
-            a=m*b;
-        System.out.println(b+" * "+m+" =" + a);}
-
-
-
+        int i=0;
+        for (; total < 2_459_000;i++) {
+             total = total + salary;}
+            System.out.println("Для накопления потребуется " +i+ " месяца");
 
     }
 }
