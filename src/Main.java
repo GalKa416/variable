5a1b6ffe4f37cf017601d25ea2d62198abcbefc7
Галина <g416@mail.ru>
2023-04-05 23:08:34 +0500

    задача 1 готова

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,11 @@ public class Main {
         System.out.println("Задача 1");
         int salary = 15000;
         int total = 0;
-        int i=0;
-        for (; total < 2_459_000;i++) {
-             total = total + salary;}
-            System.out.println("Для накопления потребуется " +i+ " месяца");
+        int i =0;
+            while ( total < 2_459_000) {
+             total = total + salary;
+            i = i+1;
+            System.out.println("Месяц  " +i+ ", сумма накоплений равна: "+total);}
 
     }
 }
